99e06a9ec1a6503e9a23c5e5e511a0374f96c193
LeechenLove <727084494@qq.com>
2020-06-13 22:54:30 +0800

    updata model

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
+import os, sys
+import click
 from flask import Flask, escape, url_for
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 
-name = 'LeechenLove'
-movies = [
+# 数据库表User类
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(20)) # 名字
+
+# 数据库表Movie类
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    title = db.Column(db.String(60)) # 电影标题
+    year = db.Column(db.String(4)) # 电影年份
+
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项
+def initdb(drop):
+    """
+    Initialize the database.
+    :param drop: 输入drop命令，表示删掉数据库表重建
+    :return:
+    """
+    if drop: # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+
+@app.cli.command()
+def forge():
+    """
+    Generate fake data.
+    :return:
+    """
+    db.create_all
+
+    # 数据
+    name = 'LeechenLove'
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -15,26 +61,24 @@ movies = [
     {'title': 'Devils on the Doorstep', 'year': '1999'},
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
-]
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % escape(name)
-
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='wen'))
-    print(url_for('user_page', name='lee'))
-    print(url_for('test_url_for'))
-    # 传入多余的参数
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
 
 if __name__ == '__main__':
     app.run()
